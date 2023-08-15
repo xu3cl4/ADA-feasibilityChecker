890514f35431c460d0563690a6687ede1ec1efa1
Hao Lee <hleeh@br011.ib.bridges2.psc.edu>
2023-08-15 12:09:32 -0400

    try to recover codes

--- utils/plot.py
+++ utils/plot.py
@@ -1,19 +1,28 @@
+paras2string = {
+        
+        }
+
+units = {
+        
+        }
 
 
-
-def modifylabels(ax, unit1, unit2, name1, name2, xlabel=True, ylabel=True, zlabel=False):
+def modifylabels(ax, par1, par2, par3=None, xlabel=True, ylabel=True, zlabel=False):
 
     if xlabel:
+        unit1, name1 = units[par1], paras2string[par1]
         ax.set_xlabel(f"{name1}{' (' + unit1 + ')' if len(unit1) > 0 else ''}", fontsize=8)
     else: 
         ax.set_xlabel(None) 
 
     if ylabel:
+        unit2, name2 = units[par2], paras2string[par2]
         ax.set_ylabel(f"{name2}{' (' + unit2 + ')' if len(unit2) > 0 else ''}", fontsize=8)
     else:
         ax.set_ylabel(None)
 
     if zlabel:
+        unit3, name3 = units[par3], paras2string[par3]
         ax.set_zlabel(f"{name2}{' (' + unit2 + ')' if len(unit2) > 0 else ''}", fontsize=8)
     else:
         ax.set_zlabel(None)
@@ -50,35 +59,31 @@ def modifyticks(ax, xtick=True, ytick=True, ztick=False):
         ax.set_zticks([]) 
     return 
 
-def addBinPlot(axs, idx, par1, par2, unit1, unit2, name1, name2, para_samples, fig, xlabel=True, ylabel=True, xtick=True, ytick=True, colorbar=True):
+def addBinPlot(ax, para_samples, par1, par2, fig, xlabel=True, ylabel=True, xtick=True, ytick=True, colorbar=True):
     '''
         Given the axis of a subplot, the function helps adding a scatter plot onto it. 
 
-        axs:   the axes of all plots 
-        idx:   the index of subplot on which we would like to add a scatter plot 
+        ax:   the axe of a plot 
+        idx:   the index of subplot on which we would like to add a bin scatter plot 
         par1:  the first parameter 
         par2:  the second parameter 
-        unit1: the unit of the first parameter 
-        unit2: the unit of the second parameter 
-        name1: the human-writtable name of the first parameter 
-        name2: the human-writtable name of the second parameter
         
         para_samples: a pandas dataframe of which each row is a parameter sample used in the simulations
     '''
     
     df = para_samples.groupby([f'{par1}_bin', f'{par2}_bin']).mean()
-    pcm = axs[idx].scatter(df.index.get_level_values(f'{par1}_bin'), df.index.get_level_values(f'{par2}_bin'), c=df['feasible'], s=100, marker='s')
-    if colorbar: fig.colorbar(pcm, ax=axs[idx])
+    pcm = ax.scatter(df.index.get_level_values(f'{par1}_bin'), df.index.get_level_values(f'{par2}_bin'), c=df['feasible'], s=100, marker='s')
+    if colorbar: fig.colorbar(pcm, ax=ax)
 
-    modifylabels(axs, idx, unit1, unit2, name1, name2, xlabel, ylabel)
-    modifyticks(axs, idx, unit1, unit2, name1, name2, xtick, ytick)
+    modifylabels(ax, par1, par2, xlabel, ylabel)
+    modifyticks(ax, par1, par2, xtick, ytick)
     return 
 
 def addScatterPlot(ax, para_samples, par1, par2, xlabel=True, ylabel=True, xtick=True, ytick=True):
     '''
         Given the axis of a subplot, the function helps adding a scatter plot onto it. 
 
-        axs:   the axes of all plots 
+        ax:   the axe of a plot 
         idx:   the index of subplot on which we would like to add a scatter plot 
         par1:  the first parameter 
         par2:  the second parameter 
